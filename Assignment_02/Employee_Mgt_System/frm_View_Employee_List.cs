0ae8c82f695b1506d6d2fde30df950d883e21a96
DhanashreePawar46177 <DhanashreePawar46177@gmail.com>
2023-06-06 09:55:32 +0530

    Assignment 02

---
+++ Assignment_02/Employee_Mgt_System/frm_View_Employee_List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee_Mgt_System
+{
+    public partial class frm_View_Employee_List : Form
+    {
+        public frm_View_Employee_List()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_View_Employee_List_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'employee_Management_System_DBDataSet1.Employee_Details' table. You can move, or remove it, as needed.
+            this.employee_DetailsTableAdapter1.Fill(this.employee_Management_System_DBDataSet1.Employee_Details);
+            // TODO: This line of code loads data into the 'employee_Management_System_DBDataSet.Employee_Details' table. You can move, or remove it, as needed.
+            this.employee_DetailsTableAdapter.Fill(this.employee_Management_System_DBDataSet.Employee_Details);
+
+            lbl_UName.Text = Common_Content.Log_Name;
+        }
+
+        private void btn_Add_New_Employee_Click(object sender, EventArgs e)
+        {
+            frm_Add_New_Employee Obj = new frm_Add_New_Employee();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void btn_LogOut_Click(object sender, EventArgs e)
+        {
+            frm_Login Obj = new frm_Login();
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
